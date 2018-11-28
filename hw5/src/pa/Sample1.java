c30950ce89b6905ada18d932b1cd6ed7d6ccbdd1
t105820036 <t105820036@ntut.org.tw>
2018-11-28 21:54:46 +0800

    hw5

---
+++ hw5/src/pa/Sample1.java
@@ -0,0 +1,13 @@
+package pa;
+
+import pa.Car;
+
+public class Sample1 {
+
+    public static void main (String[] args)
+    {
+        Car car1;
+        car1 = new Car();
+        car1.show();
+    }
+}
---
+++ hw5/src/test_p10/pb/Sample5_3.java
@@ -0,0 +1,11 @@
+package test_p10.pb;
+
+import test_p10.pc.Car;
+
+public class Sample5_3 {
+    public static void main(String[] args)
+    {
+        Car car1 = new Car();
+        car1.show();
+    }
+}
